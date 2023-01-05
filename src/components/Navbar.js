6da4872035f18fecc3fc1bd7a3a2463e415d8808
steve-ux <exe.universal@gmail.com>
2023-01-04 23:03:59 -0300

    se agregó logo, se terminó el header y el navbar, se hicieron responsive

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,54 @@
-function Navbar() {
+import Fox from "../assets/LogoFox.png";
+
+// function Navbar() {
+//     return (
+//     <div className="burgerNav">
+//   <nav class="navbar">
+//       <div class="navbar-container container">
+//       <h1 class="logo"><img class="fox" src={Fox} alt="Logo mendowebx"/>MENDOWEBX</h1>
+//         <input type="checkbox" name="" id=""/>
+//           <div class="hamburger-lines">
+//               <span class="line line1"></span>
+//               <span class="line line2"></span>
+//               <span class="line line3"></span>
+//           </div>
+//           <ul class="menu-items">
+//               <li><a href="#home">INICIO</a></li>
+//               <li><a href="#about">NOSOTROS</a></li>
+//               <li><a href="#contact">CONTACTO</a></li>
+//               <li><a href="#blog">BLOG</a></li>
+//           </ul>
+          
+//       </div>
+//   </nav>
+//     </div>
+//     );
+//   }
+  
+//   export default Navbar;
+
+  function Navbar() {
     return (
     <div className="burgerNav">
-  <nav class="navbar">
-      <div class="navbar-container container">
-      <h1 class="logo">MENDOWEBX</h1>
-        <input type="checkbox" name="" id=""/>
-          <div class="hamburger-lines">
-              <span class="line line1"></span>
-              <span class="line line2"></span>
-              <span class="line line3"></span>
-          </div>
-          <ul class="menu-items">
-              <li><a href="#home">INICIO</a></li>
-              <li><a href="#about">NOSOTROS</a></li>
-              <li><a href="#contact">CONTACTO</a></li>
-              <li><a href="#blog">BLOG</a></li>
-          </ul>
-          
-      </div>
-  </nav>
+      <h1 class="logo"><img class="fox" src={Fox} alt="Logo mendowebx" width="30"/> MENDOWEBX</h1>
+  <input id="toggle" type="checkbox"/>
+
+<label for="toggle" class="hamburger">
+  <div class="top-bun"></div>
+  <div class="meat"></div>
+  <div class="bottom-bun"></div>
+</label>
+
+<div class="nav">
+  <div class="nav-wrapper">
+    <nav>
+      <a href="#Inicio">INICIO</a><br/>
+      <a href="#Nosotros">NOSOTROS</a><br/>
+      <a href="#Contacto">CONTACTO</a><br/>
+      <a href="#Blog">BLOG</a>
+    </nav>
+  </div>
+</div>
     </div>
     );
   }
