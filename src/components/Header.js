e9f232f42d417f0268c2dd3feeb3d91a3acf9a87
steve-ux <exe.universal@gmail.com>
2023-03-13 01:06:09 -0300

    se terminó el header que quedaría por el momento, se agregó ícono y un video de fondo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,33 @@
 import Boton from "../components/Boton";
 import Navbar from "../components/Navbar";
+import Videoloop from "../assets/tv-pixel.mp4";
+import Flecha from "../assets/Arrow-down.png";
 
 function Header() {
   return (
     <div className="headerPadre">
+       <video className="video" autoPlay loop muted>
+      <source src={Videoloop} type="video/mp4" />
+      </video>
 <Navbar/>
   <div className="header">
 <h1>
-  <span style={{"--start-color":"#02DF71", "--end-color":"#2A6FF1", "--content": "'Diseñar.'", "--delay":"0s"}}>
+  <span style={{"--start-color":"#02DF71", "--end-color":"#02DF71", "--content": "'Diseñar.'", "--delay":"0s"}}>
     Diseñar.
   </span>
-  <span style={{"--start-color":"#2A6FF1","animation-name":"a2", "--end-color":"#02DF71", "--content": "'Programar.'", "--delay":"1.5s"}}>
+  <span style={{"--start-color":"#02DF71","animation-name":"a2", "--end-color":"#02DF71", "--content": "'Programar.'", "--delay":"1.5s"}}>
     Programar.
   </span>
-  <span style={{"--start-color":"#02DF71", "animation-name":"a3", "--end-color":"#2A6FF1", "--content": "'Publicar.'", "--delay":"3s"}}>
+  <span style={{"--start-color":"#02DF71", "animation-name":"a3", "--end-color":"#02DF71", "--content": "'Publicar.'", "--delay":"3s"}}>
     Publicar.
   </span>
 </h1>
-<h2>Entrá al universo de las webs, internet te espera.</h2>
+<h2 style={{"visibility":"hidden"}}>.</h2>
   
 <Boton/>
+<div className="divFlecha">
+<img src={Flecha} alt="Imagen flechas" width="50" className="flecha"/>
+</div>
   </div>
 </div>
   );
